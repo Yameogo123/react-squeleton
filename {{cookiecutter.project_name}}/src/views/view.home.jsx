a8257217a9c2de1022327d22a028cbd00867c796
Wendyam <yameogoivan10@gmail.com>
2025-02-09 22:07:31 +0100

    [UPDATED] package-lock.json must not be in the code repo

--- {{cookiecutter.project_name}}/src/views/view.home.jsx
+++ {{cookiecutter.project_name}}/src/views/view.home.jsx
@@ -1,14 +1,14 @@
 
 import React, { useState, useMemo } from "react";
-import { useSelector } from "react-redux";
-import { toast } from "react-toastify";
+//import { useSelector } from "react-redux";
+//import { toast } from "react-toastify";
 
 
 
 export default function Home() {
 
-    const user= useSelector((state)=> state.userReducer.user);
-    const token= useSelector((state)=> state.userReducer.token);
+    // const user= useSelector((state)=> state.userReducer.user);
+    // const token= useSelector((state)=> state.userReducer.token);
 
 
     // useMemo(async () => {
