0fedc89a541b88363ceb1732d4c295bffa74e829
Wendyam <yameogoivan10@gmail.com>
2025-02-09 22:04:54 +0100

    [ADDED] hooks services controllers routing security_views added basically

--- {{cookiecutter.project_name}}/src/index.js
+++ {{cookiecutter.project_name}}/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './css/index.css';
 import App from './App';
-// import reportWebVitals from './tests/reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+import Provide from "./hooks/hooks.provider"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provide>
+        <App />
+      </Provide>
+    </Router>
   </React.StrictMode>
 );
 
---
+++ {{cookiecutter.project_name}}/src/views/view.home.jsx
@@ -0,0 +1,28 @@
+
+import React, { useState, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+
+
+export default function Home() {
+
+    const user= useSelector((state)=> state.userReducer.user);
+    const token= useSelector((state)=> state.userReducer.token);
+
+
+    // useMemo(async () => {
+    //     let res = await getUsersOfCompany(user?.company, token);
+    //     if(res !== -1){ 
+    //         setUsers(res);
+    //     }
+    // }, [token, user?.company]);
+
+    
+
+    return (
+        <div className="content-wrapper">
+            home
+        </div>
+    )
+}
